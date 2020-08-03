34bdc8ad3ec97bab77a26ae22e51df95a0474923
Toby Harris <tobyharris93@gmail.com>
2020-08-03 10:33:34 +1000

    /tt

---
+++ Modules/Commands.lua
@@ -0,0 +1,19 @@
+local Weave = select(2, ...)
+local Console = LibStub("AceConsole-3.0")
+
+local Commands = {}
+Weave.Commands = Commands
+
+function Commands:TellTarget(message)
+  if UnitIsPlayer("target") ~= true then return end
+  local target = UnitName("target")
+  local language = GetDefaultLanguage("player")
+  SendChatMessage(message, "WHISPER", language, target);
+end
+
+
+-- Register 
+local Console = LibStub("AceConsole-3.0")
+Console:RegisterChatCommand("tt", function(message)
+  Commands:TellTarget(message)
+end)
--- Modules/Theme.lua
+++ Modules/Theme.lua
@@ -4,15 +4,20 @@ local T, C, L = Tukui:unpack()
 Weave.Theme = CreateFrame("Frame")
 local Theme = Weave.Theme
 
+function Theme:SetConfigDefaults()
+	-- print("SetConfigDefaults")
+end
+
 function Theme:SwitchActionBars()
-	Weave.ActionBars:SwitchButtons(T.Panels.ActionBar2, "MultiBarRight")
+	-- Weave.ActionBars:SwitchButtons(T.Panels.ActionBar2, "MultiBarRight")
 	-- WActionBars:SwitchButtons(T.Panels.ActionBar3, "MultiBarBottomRight")
 	-- WActionBars:SwitchButtons(T.Panels.ActionBar4, "MultiBarBottomLeft")
 end
 
 function Theme:OnEvent(event)
-  if (C.General.Themes.Value == "WeaveUI") then
-		self:SwitchActionBars()
+	if (C.General.Themes.Value == "WeaveUI") then
+		self:SetConfigDefaults()
+		-- self:SwitchActionBars()
 	end
 end
 
